e6730ade0880add5e5e40bdf654f5499090097b5
vipajuhi <vipajuhi@hotmail.com>
2020-02-21 11:45:05 -0500

    Create SimpleInterest.py

---
+++ SimpleInterest.py
@@ -0,0 +1,12 @@
+# Calculate simple interest 
+  
+
+Princ = 1 
+Rate = 1 
+Time = 1 
+  
+# Calculates simple interest  
+SimpI = (Princ * Rate * Time) / 100
+  
+# Print the resultant value of Simple Interest
+print("Simple Interest is", SimpI) 
